b9af3290b617eb436be31226d5fd8a89f2b3424e
joshuamkite <joshuamkite@gmail.com>
2019-03-10 22:21:41 +0000

    quote variable names

--- variables.tf
+++ variables.tf
@@ -14,56 +14,56 @@
  * limitations under the License.
  */
 
-variable module_enabled {
+variable "module_enabled" {
   description = ""
   default     = true
 }
 
-variable project {
+variable "project" {
   description = "The project to deploy to, if not set the default provider project is used."
   default     = ""
 }
 
-variable region {
+variable "region" {
   description = "Region for cloud resources."
   default     = "us-central1"
 }
 
-variable zone {
+variable "zone" {
   description = "Zone for managed instance groups."
   default     = "us-central1-f"
 }
 
-variable network {
+variable "network" {
   description = "Name of the network to deploy instances to."
   default     = "default"
 }
 
-variable subnetwork {
+variable "subnetwork" {
   description = "The subnetwork to deploy to"
   default     = "default"
 }
 
-variable subnetwork_project {
+variable "subnetwork_project" {
   description = "The project the subnetwork belongs to. If not set, var.project is used instead."
   default     = ""
 }
 
-variable name {
+variable "name" {
   description = "Name of the managed instance group."
 }
 
-variable size {
+variable "size" {
   description = "Target size of the managed instance group."
   default     = 1
 }
 
-variable startup_script {
+variable "startup_script" {
   description = "Content of startup-script metadata passed to the instance template."
   default     = ""
 }
 
-variable access_config {
+variable "access_config" {
   description = "The access config block for the instances. Set to [] to remove external IP."
   type        = "list"
 
@@ -72,90 +72,90 @@ variable access_config {
   ]
 }
 
-variable metadata {
+variable "metadata" {
   description = "Map of metadata values to pass to instances."
   type        = "map"
   default     = {}
 }
 
-variable can_ip_forward {
+variable "can_ip_forward" {
   description = "Allow ip forwarding."
   default     = false
 }
 
-variable network_ip {
+variable "network_ip" {
   description = "Set the network IP of the instance in the template. Useful for instance groups of size 1."
   default     = ""
 }
 
-variable machine_type {
+variable "machine_type" {
   description = "Machine type for the VMs in the instance group."
   default     = "f1-micro"
 }
 
-variable compute_image {
+variable "compute_image" {
   description = "Image used for compute VMs."
   default     = "projects/debian-cloud/global/images/family/debian-9"
 }
 
-variable wait_for_instances {
+variable "wait_for_instances" {
   description = "Wait for all instances to be created/updated before returning"
   default     = false
 }
 
-variable update_policy {
+variable "update_policy" {
   description = "The upgrade policy to apply when the instance template changes."
   type        = "list"
   default     = []
 }
 
-variable service_port {
+variable "service_port" {
   description = "Port the service is listening on."
 }
 
-variable service_port_name {
+variable "service_port_name" {
   description = "Name of the port the service is listening on."
 }
 
-variable target_tags {
+variable "target_tags" {
   description = "Tag added to instances for firewall and networking."
   type        = "list"
   default     = ["allow-service"]
 }
 
-variable instance_labels {
+variable "instance_labels" {
   description = "Labels added to instances."
   type        = "map"
   default     = {}
 }
 
-variable target_pools {
+variable "target_pools" {
   description = "The target load balancing pools to assign this group to."
   type        = "list"
   default     = []
 }
 
-variable depends_id {
+variable "depends_id" {
   description = "The ID of a resource that the instance group depends on."
   default     = ""
 }
 
-variable local_cmd_create {
+variable "local_cmd_create" {
   description = "Command to run on create as local-exec provisioner for the instance group manager."
   default     = ":"
 }
 
-variable local_cmd_destroy {
+variable "local_cmd_destroy" {
   description = "Command to run on destroy as local-exec provisioner for the instance group manager."
   default     = ":"
 }
 
-variable service_account_email {
+variable "service_account_email" {
   description = "The email of the service account for the instance template."
   default     = "default"
 }
 
-variable service_account_scopes {
+variable "service_account_scopes" {
   description = "List of scopes for the instance template service account"
   type        = "list"
 
@@ -167,7 +167,7 @@ variable service_account_scopes {
   ]
 }
 
-variable zonal {
+variable "zonal" {
   description = "Create a single-zone managed instance group. If false, a regional managed instance group is created."
   default     = true
 }
@@ -178,28 +178,28 @@ variable distribution_policy_zones {
   default     = []
 }
 
-variable ssh_source_ranges {
+variable "ssh_source_ranges" {
   description = "Network ranges to allow SSH from"
   type        = "list"
   default     = ["0.0.0.0/0"]
 }
 
-variable disk_auto_delete {
+variable "disk_auto_delete" {
   description = "Whether or not the disk should be auto-deleted."
   default     = true
 }
 
-variable disk_type {
+variable "disk_type" {
   description = "The GCE disk type. Can be either pd-ssd, local-ssd, or pd-standard."
   default     = "pd-ssd"
 }
 
-variable disk_size_gb {
+variable "disk_size_gb" {
   description = "The size of the image in gigabytes. If not specified, it will inherit the size of its base image."
   default     = 0
 }
 
-variable mode {
+variable "mode" {
   description = "The mode in which to attach this disk, either READ_WRITE or READ_ONLY."
   default     = "READ_WRITE"
 }
@@ -215,86 +215,86 @@ variable "automatic_restart" {
 }
 
 /* Autoscaling */
-variable autoscaling {
+variable "autoscaling" {
   description = "Enable autoscaling."
   default     = false
 }
 
-variable max_replicas {
+variable "max_replicas" {
   description = "Autoscaling, max replicas."
   default     = 5
 }
 
-variable min_replicas {
+variable "min_replicas" {
   description = "Autoscaling, min replics."
   default     = 1
 }
 
-variable cooldown_period {
+variable "cooldown_period" {
   description = "Autoscaling, cooldown period in seconds."
   default     = 60
 }
 
-variable autoscaling_cpu {
+variable "autoscaling_cpu" {
   description = "Autoscaling, cpu utilization policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler.html#cpu_utilization"
   type        = "list"
   default     = []
 }
 
-variable autoscaling_metric {
+variable "autoscaling_metric" {
   description = "Autoscaling, metric policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler.html#metric"
   type        = "list"
   default     = []
 }
 
-variable autoscaling_lb {
+variable "autoscaling_lb" {
   description = "Autoscaling, load balancing utilization policy block as single element array. https://www.terraform.io/docs/providers/google/r/compute_autoscaler.html#load_balancing_utilization"
   type        = "list"
   default     = []
 }
 
 /* Health checks */
-variable http_health_check {
+variable "http_health_check" {
   description = "Enable or disable the http health check for auto healing."
   default     = true
 }
 
-variable hc_initial_delay {
+variable "hc_initial_delay" {
   description = "Health check, intial delay in seconds."
   default     = 30
 }
 
-variable hc_interval {
+variable "hc_interval" {
   description = "Health check, check interval in seconds."
   default     = 30
 }
 
-variable hc_timeout {
+variable "hc_timeout" {
   description = "Health check, timeout in seconds."
   default     = 10
 }
 
-variable hc_healthy_threshold {
+variable "hc_healthy_threshold" {
   description = "Health check, healthy threshold."
   default     = 1
 }
 
-variable hc_unhealthy_threshold {
+variable "hc_unhealthy_threshold" {
   description = "Health check, unhealthy threshold."
   default     = 10
 }
 
-variable hc_port {
+variable "hc_port" {
   description = "Health check, health check port, if different from var.service_port, if not given, var.service_port is used."
   default     = ""
 }
 
-variable hc_path {
+variable "hc_path" {
   description = "Health check, the http path to check."
   default     = "/"
 }
 
-variable ssh_fw_rule {
+variable "ssh_fw_rule" {
   description = "Whether or not the SSH Firewall Rule should be created"
   default     = true
 }
